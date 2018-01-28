a3c4ab50cfa9e0b564f70839e29b6345ff502b10
Glitch (star-crocus) <none>
2018-01-28 05:24:03 +0000

    Import of AkikoKanzaki/meyasubakot

--- scripts/slack_bot.js
+++ scripts/slack_bot.js
@@ -1,13 +1,15 @@
-controller.on('direct_message',function(bot,message) {
+ 
+  controller.on('direct_message',function(bot,message) {
   bot.reply(message,"匿名で投稿しました:mailbox_with_mail:");
-
-  // 投稿先がユーザーの場合
-  bot.startPrivateConversation({user: "U81U6MKNG"}, function(response, convo){
+  
+    // 投稿先がユーザーの場合
+      bot.startPrivateConversation({user: "U81U6MKNG"}, function(response, convo){
     convo.say(message);
-  })
-
-  // 投稿先がチャンネルの場合
+      })
+      
+    // 投稿先がチャンネルの場合
   bot.startConversation({channel: "\u7df4\u7fd2\u5834"}, function(response, convo){
     convo.say(message);
   })
 });
+  
